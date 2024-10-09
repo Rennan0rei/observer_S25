fa118170f2aa11fbf86b4d7a5404b3f42000f361
Henrique F <hefesi93@gmail.com>
2024-10-09 16:12:46 -0300

    Criação do consumer

---
+++ Consumer/consumer.py
@@ -0,0 +1,22 @@
+from observables.observer import Observer
+from producer import Producer
+class Consumer(Observer):
+    def update(self, message):
+        # Simulando processamento da mensagem
+        print(f"Consumer processando a mensagem: {message}")
+        self.consume_message(message)
+    
+    def consume_message(self, message):
+        # Processamento específico da mensagem
+        print(f"Mensagem consumida: {message}")
+
+# Uso
+if __name__ == "__main__":
+    producer = Producer()
+    consumer = Consumer()
+    
+    # Registrando o consumidor (observer)
+    producer.register_observer(consumer)
+
+    # Produzindo uma mensagem
+    producer.produce_message("Mensagem para o consumidor")
