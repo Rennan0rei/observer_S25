6fb2e45f0c0121208c03b6072934b83bb541c417
Lanna Delmondes <00001085525120sp@al.educacao.sp.gov.br>
2024-10-09 16:29:04 -0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-dadwad
+from Producer.producer import Producer
+from Consumer.consumer import Consumer
+
+def main():
+    # Criando instâncias do Producer e do Consumer
+    producer = Producer()
+    consumer1 = Consumer()
+    consumer2 = Consumer()
+
+    # Registrando os consumidores
+    producer.register_observer(consumer1)
+    producer.register_observer(consumer2)
+
+    # Produzindo algumas mensagens
+    producer.produce_message("Olá, mundo do consumidor!")
+    producer.produce_message("Mensagem adicional para os consumidores.")
+
+if __name__ == "_main_":
+    main()
